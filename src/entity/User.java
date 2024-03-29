a101e428005cd6ac2737d35e0e6a04e8073f92b0
AshkanArabim <ashkan.arabim@gmail.com>
2024-03-28 22:26:54 -0600

    update User

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,17 +1,37 @@
 package entity;
 
 public class User extends Person {
-    
+
+    private String userName;
+
+    private String password;
+
     @Override
     public void login() {
         System.out.println("Logging in");
     }
 
-    public void viewCars() {}
+    // getter & setters
 
-    public void purchaseCars() {}
+    public boolean isMember() {
+        return true;
+    }
 
-    public void applyForMembershipI() {}
-    
-    public void viewTicket() {}
+    // methods
+
+    public void viewCars() {
+    }
+
+    public void purchaseCars() {
+    }
+
+    public void filterCars() {
+    }
+
+    public void viewTicket() {
+    }
+
+    public void applyForMembership() {
+    }
+
 }
