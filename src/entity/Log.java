770d0bb216dc6a819e2441e75205efafd23324c6
riddle-master-ruben <119084862+riddle-master-ruben@users.noreply.github.com>
2024-04-06 19:35:26 -0600

    Moved getters to bottom for consistency

--- src/entity/Log.java
+++ src/entity/Log.java
@@ -37,6 +37,47 @@ public class Log {
         }
     }
 
+    
+    // public methods
+    
+    /**
+     * Adds a log entry to the log file.
+     * BE SURE TO CALL THIS FOR EVERY OPTION AVAILABLE TO USERS!!
+     * Each log entry will use the following format:
+     * `date, time, username, action, description`
+     * 
+     * @param action      The action performed. Keep it two words or less.
+     * @param description More details about the action performed, including the
+     *                    subject.
+     *                    For example, if user bought car X, simply write "User
+     *                    bought car <carname>".
+     *                    DO NOT explain what an action is. That's just clutter.
+     */
+    public void addLogEntry(String action, String description) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter datefmt = DateTimeFormatter.ISO_LOCAL_DATE;
+        DateTimeFormatter timefmt = DateTimeFormatter.ISO_LOCAL_TIME;
+        String toLog = now.format(datefmt) + ","
+        + now.format(timefmt) + ","
+        + this.username + ","
+        + action;
+        
+        // add extra comma & description if needed
+        if (description.length() > 0) {
+            toLog += "," + description;
+        }
+        
+        toLog += "\n";
+        
+        try {
+            this.logWriter.write(toLog);
+            this.logWriter.flush();
+        } catch (Exception e) {
+            System.out.println("Error while writing to log file!");
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
     // getters
 
     public String getDatadir() {
@@ -59,46 +100,5 @@ public class Log {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    // public methods
-
-    /**
-     * Adds a log entry to the log file.
-     * BE SURE TO CALL THIS FOR EVERY OPTION AVAILABLE TO USERS!!
-     * Each log entry will use the following format:
-     * `date, time, username, action, description`
-     * 
-     * @param action      The action performed. Keep it two words or less.
-     * @param description More details about the action performed, including the
-     *                    subject.
-     *                    For example, if user bought car X, simply write "User
-     *                    bought car <carname>".
-     *                    DO NOT explain what an action is. That's just clutter.
-     */
-    public void addLogEntry(String action, String description) {
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter datefmt = DateTimeFormatter.ISO_LOCAL_DATE;
-        DateTimeFormatter timefmt = DateTimeFormatter.ISO_LOCAL_TIME;
-        String toLog = now.format(datefmt) + ","
-                + now.format(timefmt) + ","
-                + this.username + ","
-                + action;
-
-        // add extra comma & description if needed
-        if (description.length() > 0) {
-            toLog += "," + description;
-        }
-
-        toLog += "\n";
-
-        try {
-            this.logWriter.write(toLog);
-            this.logWriter.flush();
-        } catch (Exception e) {
-            System.out.println("Error while writing to log file!");
-            e.printStackTrace();
-            System.exit(1);
-        }
     }
 }
